a4675b7845c57c3c53039100d2cd2bfdf7620ef2
tobiwall <t.w.95@web.de>
2024-06-24 11:33:02 +0200

    work on responsive and image comprimised

--- src/app/share/header/header.component.ts
+++ src/app/share/header/header.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isHeaderOpen = false;
+  isWideScreen = false;
 
+  ngOnInit() {
+    this.checkScreenWidth();
+  }
+
+  openHeaderBar() {
+    this.isHeaderOpen = !this.isHeaderOpen;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkScreenWidth();
+  }
+
+  checkScreenWidth() {
+    this.isWideScreen = window.innerWidth > 750;
+    if (this.isWideScreen) {
+      this.isHeaderOpen = false;
+    }
+  }
 }
