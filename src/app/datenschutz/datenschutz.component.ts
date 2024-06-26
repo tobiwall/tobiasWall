2cd87326013e8e2a68dfa12a3fda1a9bcbbb2c5f
tobiwall <t.w.95@web.de>
2024-06-26 23:09:00 +0200

    translate

--- src/app/datenschutz/datenschutz.component.ts
+++ src/app/datenschutz/datenschutz.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-datenschutz',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, TranslateModule],
   templateUrl: './datenschutz.component.html',
   styleUrl: './datenschutz.component.scss'
 })
